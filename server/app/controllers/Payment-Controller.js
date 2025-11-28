a9540e3f96d7841765a6cfbd512192f566dfe094
Sadhuanushya <sadhuanushya08@gmail.com>
2025-11-28 20:58:51 +0530

    modified payment controller

--- server/app/controllers/Payment-Controller.js
+++ server/app/controllers/Payment-Controller.js
@@ -1,12 +1,17 @@
 
+const Subscription=require("../models/payment-model");
 const instance= require("../utils/Instance");
 const PaymentCtrl={};
 PaymentCtrl.pay=(async(req,res)=>{
     const options={
-        amount:Number(req.body.amount*100),
+        amount:Number(req.body.amount),
         currency:"INR"
     }
     const order=await instance.orders.create(options)
-    res.status(200).json({success:true,order})
+    if(order){
+        const payment=new Subscription(req.body);
+        await payment.save();
+        res.status(201).json(payment)   
+    }
 })
 module.exports=PaymentCtrl;
