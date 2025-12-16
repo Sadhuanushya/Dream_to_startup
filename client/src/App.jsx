71524204414574fa0043f2c9dbb23e17724aec8e
Sadhuanushya <sadhuanushya08@gmail.com>
2025-12-16 22:35:03 +0530

    update routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,28 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import DashBoard from './pages/DashBoard';
-
+import Home from "../src/pages/Home";
+import Notifications from './pages/Notifications';
+import Account from "./pages/Account"
+import Investers from "./pages/Investers"
+import Entrepreneurs from "./pages/Entrepreneurs"
+import Pinch from "./pages/Pinch"
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path="/" element={<Register />} />
+        <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<DashBoard/>}/>
+        <Route path="/account" element={<Account/>}/>
+
+        <Route path="/investers" element={<Investers/>}/>
+
+        <Route path ="/entrepreneurs" element={<Entrepreneurs/>}/>
+    
+        <Route path="/pinch" element={<Pinch/>}/>
+        <Route path="/notifications" element={<Notifications/>}/>
       </Routes>
     </BrowserRouter>
   );
